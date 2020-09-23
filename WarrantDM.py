3c47d457f43e26164d2d9eb2901b7c5022cdcb27
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-09-23 17:50:23 -0600

    Update WarrantDM.py

--- WarrantDM.py
+++ WarrantDM.py
@@ -31,11 +31,11 @@ async def dm(ctx, user_name:discord.Member, *,args):
     print(f"{ctx.message.author} sent {user_name} a DM {args}")
     try:
         await user_name.send(args)
+        embed=discord.Embed(title="DM Sent", description=f"**Sent From:** {ctx.message.author} \n**Sent To:** {user_name} \n**Message:**\n{args}")
+        await ctx.send(embed=embed)
     except Exception as e:
         embed=discord.Embed(title="DM Failed", description=f"**Error:** {e}")
         await ctx.send(embed=embed)
-    embed=discord.Embed(title="DM Sent", description=f"**Sent From:** {ctx.message.author} \n**Sent To:** {user_name} \n**Message:**\n{args}")
-    await ctx.send(embed=embed)
 
 with open(os.path.join(sys.path[0], "config.json"), "r") as f:
     data = json.load(f)
